d00ed975ef9cf232d6e637f8c084f5841d19847c
Vlad Croicu (Zoli) <101589800+Zolianul@users.noreply.github.com>
2024-05-12 20:12:24 +0300

    Create StreamView.java
    view the flask stream functionality and logic

---
+++ app/src/main/java/com/example/insight/StreamView.java
@@ -0,0 +1,23 @@
+package com.example.insight;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class StreamView extends AppCompatActivity {
+    private WebView mWebView;
+    private Button captureButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stream_view);
+
+        mWebView= (WebView) findViewById(R.id.webview);
+        mWebView.setWebViewClient(new WebViewClient());
+        //mWebView.loadUrl("http://192.168.90.205:5000");
+        mWebView.loadUrl("http://192.168.252.64:5000");
+    }
+}
